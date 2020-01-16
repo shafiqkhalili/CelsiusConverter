eeaa886787da289a03ee8cc06ea3e8c37897641f
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-01-16 12:26:53 +0100

    init commit

---
+++ CelsiusConverter/CelsiusConverter.swift
@@ -0,0 +1,9 @@
+//
+//  CelsiusConverter.swift
+//  CelsiusConverter
+//
+//  Created by Shafigh Khalili on 2020-01-16.
+//  Copyright © 2020 Shafigh Khalili. All rights reserved.
+//
+
+import Foundation
